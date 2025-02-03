cdd7aff18f350ec811407515a8b1f35ce3679df8
Ruben Vasile Marcu Ungureanu <rvmu@rvmu.es>
2025-02-03 05:47:46 +0100

    0.0.18

--- loghid/Pages/Index.cshtml.cs
+++ loghid/Pages/Index.cshtml.cs
@@ -1,38 +1,17 @@
 using Loghid.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Loghid.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace Loghid.Pages // Namespace con casing correcto
+namespace Loghid.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ParametersService _parametersService;
-        
-        // Clase movida fuera del modelo
-        public class SubstanceTable
-        {
-            public string Name { get; set; } = string.Empty;
-            public IEnumerable<ISubstanceData> Data { get; set; } = Enumerable.Empty<ISubstanceData>();
-        }
 
-        // Propiedades simplificadas con inicialización directa
-        public IEnumerable<Water> WaterData { get; set; } = new List<Water>();
-        public IEnumerable<Methane> MethaneData { get; set; } = new List<Methane>();
-        public IEnumerable<NonCH4Hydrocarbons> NonCH4HydrocarbonsData { get; set; } = new List<NonCH4Hydrocarbons>();
-        public IEnumerable<Oxygen> OxygenData { get; set; } = new List<Oxygen>();
-        public IEnumerable<Helium> HeliumData { get; set; } = new List<Helium>();
-        public IEnumerable<Nitrogen> NitrogenData { get; set; } = new List<Nitrogen>();
-        public IEnumerable<Argon> ArgonData { get; set; } = new List<Argon>();
-        public IEnumerable<CarbonDioxide> CarbonDioxideData { get; set; } = new List<CarbonDioxide>();
-        public IEnumerable<CarbonMonoxide> CarbonMonoxideData { get; set; } = new List<CarbonMonoxide>();
-        public IEnumerable<SulphurCompounds> SulphurCompoundsData { get; set; } = new List<SulphurCompounds>();
-        public IEnumerable<Formaldehyde> FormaldehydeData { get; set; } = new List<Formaldehyde>();
-        public IEnumerable<FormicAcid> FormicAcidData { get; set; } = new List<FormicAcid>();
-        public IEnumerable<Ammonia> AmmoniaData { get; set; } = new List<Ammonia>();
-        public IEnumerable<HalogenatedCompounds> HalogenatedCompoundsData { get; set; } = new List<HalogenatedCompounds>();
-        public IEnumerable<Hydrocarbons> HydrocarbonsData { get; set; } = new List<Hydrocarbons>();
-
-        // ... Repetir para todas las propiedades restantes ...
+        public IEnumerable<SubstanceDisplayModel> Substances { get; set; } = new List<SubstanceDisplayModel>();
 
         public IndexModel(ParametersService parametersService)
         {
@@ -46,28 +25,43 @@ namespace Loghid.Pages // Namespace con casing correcto
 
         private async Task LoadAllDataAsync()
         {
-            WaterData = await _parametersService.GetWaterDataAsync();
-            MethaneData = await _parametersService.GetMethaneDataAsync();
-            NonCH4HydrocarbonsData = await _parametersService.GetNonCH4HydrocarbonsDataAsync();
-            OxygenData = await _parametersService.GetOxygenDataAsync();
-            HeliumData = await _parametersService.GetHeliumDataAsync();
-            NitrogenData = await _parametersService.GetNitrogenDataAsync();
-            ArgonData = await _parametersService.GetArgonDataAsync();
-            CarbonDioxideData = await _parametersService.GetCarbonDioxideDataAsync();
-            CarbonMonoxideData = await _parametersService.GetCarbonMonoxideDataAsync();
-            SulphurCompoundsData = await _parametersService.GetSulphurCompoundsDataAsync();
-            FormaldehydeData = await _parametersService.GetFormaldehydeDataAsync();
-            FormicAcidData = await _parametersService.GetFormicAcidDataAsync();
-            AmmoniaData = await _parametersService.GetAmmoniaDataAsync();
-            HalogenatedCompoundsData = await _parametersService.GetHalogenatedCompoundsDataAsync();
-            HydrocarbonsData = await _parametersService.GetHydrocarbonsDataAsync();
+            Substances = new List<SubstanceDisplayModel>
+            {
+                new SubstanceDisplayModel("Water", (await _parametersService.GetWaterDataAsync()).Cast<ISubstanceData>()),
+                new SubstanceDisplayModel("Methane", (await _parametersService.GetMethaneDataAsync()).Cast<ISubstanceData>()),
+                new SubstanceDisplayModel("Non-CH4 Hydrocarbons", (await _parametersService.GetNonCH4HydrocarbonsDataAsync()).Cast<ISubstanceData>()),
+                new SubstanceDisplayModel("Oxygen", (await _parametersService.GetOxygenDataAsync()).Cast<ISubstanceData>()),
+                new SubstanceDisplayModel("Helium", (await _parametersService.GetHeliumDataAsync()).Cast<ISubstanceData>()),
+                new SubstanceDisplayModel("Nitrogen", (await _parametersService.GetNitrogenDataAsync()).Cast<ISubstanceData>()),
+                new SubstanceDisplayModel("Argon", (await _parametersService.GetArgonDataAsync()).Cast<ISubstanceData>()),
+                new SubstanceDisplayModel("Carbon Dioxide", (await _parametersService.GetCarbonDioxideDataAsync()).Cast<ISubstanceData>()),
+                new SubstanceDisplayModel("Carbon Monoxide", (await _parametersService.GetCarbonMonoxideDataAsync()).Cast<ISubstanceData>()),
+                new SubstanceDisplayModel("Sulphur Compounds", (await _parametersService.GetSulphurCompoundsDataAsync()).Cast<ISubstanceData>()),
+                new SubstanceDisplayModel("Formaldehyde", (await _parametersService.GetFormaldehydeDataAsync()).Cast<ISubstanceData>()),
+                new SubstanceDisplayModel("Formic Acid", (await _parametersService.GetFormicAcidDataAsync()).Cast<ISubstanceData>()),
+                new SubstanceDisplayModel("Ammonia", (await _parametersService.GetAmmoniaDataAsync()).Cast<ISubstanceData>()),
+                new SubstanceDisplayModel("Halogenated Compounds", (await _parametersService.GetHalogenatedCompoundsDataAsync()).Cast<ISubstanceData>()),
+                new SubstanceDisplayModel("Hydrocarbons", (await _parametersService.GetHydrocarbonsDataAsync()).Cast<ISubstanceData>())
+            };
         }
     }
 
-    // Clase movida fuera del modelo para mejor acceso
-    public class SubstanceTable
+    public class SubstanceDisplayModel
     {
-        public string Name { get; set; } = string.Empty;
-        public IEnumerable<ISubstanceData> Data { get; set; } = Enumerable.Empty<ISubstanceData>();
+        public string Name { get; set; }
+        public IEnumerable<ISubstanceData> Data { get; set; }
+
+        public SubstanceDisplayModel(string name, IEnumerable<ISubstanceData> data)
+        {
+            Name = name;
+            Data = data ?? Enumerable.Empty<ISubstanceData>();
+        }
+    }
+
+    public interface ISubstanceData
+    {
+        int Id { get; set; }
+        double IsoThreshold { get; set; }
+        string Probability { get; set; }
     }
 }
