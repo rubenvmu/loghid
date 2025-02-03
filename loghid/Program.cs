51b9c1093540b3b2edb8e89a5479f4dfa78c921d
Ruben Vasile Marcu Ungureanu <rvmu@rvmu.es>
2025-02-03 07:14:31 +0100

    0.2.0

--- loghid/Program.cs
+++ loghid/Program.cs
@@ -4,19 +4,31 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Loghid.Services;
-
-
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddScoped<ParametersService>();
 builder.Services.AddRazorPages();
-
 builder.Services.AddDbContext<LoghidDbContext>(options => 
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+var lifetime = app.Services.GetRequiredService<IHostApplicationLifetime>();
+
+lifetime.ApplicationStopping.Register(() =>
+{
+    Console.WriteLine("La aplicación se está apagando.");
+});
+
+int GenerateRandomPort()
+{
+    return new Random().Next(5000, 6000);
+}
+
+var port = GenerateRandomPort();
+Console.WriteLine($"Iniciando la aplicación en el puerto: {port}");
 
 if (!app.Environment.IsDevelopment())
 {
@@ -31,4 +43,4 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
-app.Run();
+app.Run($"http://localhost:{port}");
