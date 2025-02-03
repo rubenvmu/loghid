6e2c9b1135c8c17d14f9c6f5b542278a4dc425fc
Ruben Vasile Marcu Ungureanu <rvmu@rvmu.es>
2025-02-03 11:02:14 +0100

    0.2.23

---
+++ loghid/Migrations/20250203094922_LoghidDbContext3.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace loghid.Migrations
+{
+    /// <inheritdoc />
+    public partial class LoghidDbContext3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
