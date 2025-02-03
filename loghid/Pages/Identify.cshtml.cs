d60169ceadf5817d4479eac5c75bc34ff981f2bf
Ruben Vasile Marcu Ungureanu <rvmu@rvmu.es>
2025-02-03 19:24:18 +0100

    0.5.1

---
+++ loghid/Pages/Identify.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace loghid.Pages
+{
+    public class IdentifyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public IdentifyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
---
+++ loghid/Pages/Incidents.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace loghid.Pages
+{
+    public class IncidentsModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public IncidentsModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
