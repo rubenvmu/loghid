b282b8804256b77f4d4cfcd2f0d52b58e54bf2ea
Ruben Vasile Marcu Ungureanu <rvmu@rvmu.es>
2025-02-07 22:53:03 +0100

    0.7.7

---
+++ loghid/Pages/CertificatesIndex.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Loghid.Data;
+using Loghid.Models;
+
+
+namespace Loghid.Pages
+{
+    public class CertificatesIndex : PageModel
+    {
+        private readonly Loghid.Data.LoghidClientMeasurementDbContext _context;
+
+
+        public CertificatesIndex(Loghid.Data.LoghidClientMeasurementDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<ClientMeasurement> ClientMeasurement { get; set; } = new List<ClientMeasurement>();
+
+        public async Task OnGetAsync()
+        {
+            ClientMeasurement = await _context.Measurements.ToListAsync();
+        }
+    }
+}
